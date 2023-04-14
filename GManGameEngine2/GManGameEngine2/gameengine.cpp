7957b1486ad8d8cb5a60578a057fe492edfe276a
GManTheMarioGuy <123761856+GManTheMarioGuy@users.noreply.github.com>
2023-04-14 14:10:54 -0500

    Uploaded CPP Files For GManGameEngine2

---
+++ GManGameEngine2/GManGameEngine2/gameengine.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("you can mod nl2 steps below!\n");
+	printf("go into the newest release of the gtmgf2 pack downloaded via google drive now go into the nl2_data then go into modding then there's steps on modding in unity in the readme.txt file!");
+	getchar();
+	return 0;
+}
